d748baf61daa72ca2a4e343c163950c9ac8d8f1e
Umaralixan <umaralixtreme@gmail.com>
2025-04-22 09:43:06 +0500

    index

---
+++ app.js
@@ -0,0 +1,146 @@
+// M 1
+// let k = prompt("Hafta raqamini kiriting");
+// k = Number(k);
+// switch (k) {
+//   case 1:
+//     console.log("Dushanba");
+//     break;
+//   case 2:
+//     console.log("Seshanba");
+//     break;
+//   case 3:
+//     console.log("CHorshanba");
+//     break;
+//   case 4:
+//     console.log("Payshanba");
+//     break;
+//   case 5:
+//     console.log("Juma");
+//     break;
+//   case 6:
+//     console.log("Shanba");
+//     break;
+//   case 7:
+//     console.log("Yakshanba");
+//     break;
+//   default:
+//     console.log("Noto'g'ri son kiritildi");
+// }
+
+// M 2
+// let a = prompt("Bahoni kiriting");
+// a = Number(a);
+// switch (a) {
+//   case 1:
+//     console.log("Yomomn");
+//     break;
+//   case 2:
+//     console.log("Qoniqarsiz");
+//     break;
+//   case 3:
+//     console.log("Qoniqarli");
+//     break;
+//   case 4:
+//     console.log("Yaxshi");
+//     break;
+//   case 5:
+//     console.log("A'lo");
+//     break;
+//   default:
+//     console.log("Xato");
+// }
+
+// M 3
+// let oy = prompt("Oy raqamini kiriritng");
+// oy = Number(oy);
+// switch (oy) {
+//   case 12:
+//   case 1:
+//   case 2:
+//     console.log("Qish");
+//     break;
+//   case 3:
+//   case 4:
+//   case 5:
+//     console.log("Bahor");
+//     break;
+//   case 6:
+//   case 7:
+//   case 8:
+//     console.log("Yoz");
+//     break;
+//   case 9:
+//   case 10:
+//   case 11:
+//     console.log("Kuz");
+//     break;
+//   default:
+//     console.log("Xato");
+// }
+
+// M 4
+// let kun = prompt("Oy raqamini kiriritng");
+// kun = Number(kun);
+// switch (kun) {
+//   case 1:
+//     console.log("Yanvar 31 kun");
+//     break;
+//   case 2:
+//     console.log("Fevral 28 kun");
+//     break;
+//   case 3:
+//     console.log("Mart 31 kun");
+//     break;
+//   case 4:
+//     console.log("Aprel 30 kun");
+//     break;
+//   case 5:
+//     console.log("May 31 kun");
+//     break;
+//   case 6:
+//     console.log("Iyun 30 kun");
+//     break;
+//   case 7:
+//     console.log("Iyul 31 kun");
+//     break;
+//   case 8:
+//     console.log("Avgust 31 kun");
+//     break;
+//   case 9:
+//     console.log("Sentabir 30 kun");
+//     break;
+//   case 10:
+//     console.log("Oktabr 31 kun");
+//     break;
+//   case 11:
+//     console.log("Noyabr 30 kun");
+//     break;
+//   case 12:
+//     console.log("Dekabr 31 kun");
+//     break;
+//   default:
+//     console.log("Xato");
+// }
+
+//  M 6
+// let uzinlik = prompt("Kesma raqamini kiriting");
+// uzinlik = Number(uzinlik);
+// switch (uzinlik) {
+//   case 1:
+//     console.log("1 Detsimetr = 0.1 metr");
+//     break;
+//   case 2:
+//     console.log("2 kilometr = 2000 metr");
+//     break;
+//   case 3:
+//     console.log("3 metr");
+//     break;
+//   case 4:
+//     console.log("4 millimetr = 0.004 metr");
+//     break;
+//   case 5:
+//     console.log("5 santimetr = 0.05 metr");
+//     break;
+//   default:
+//     console.log("Xato");
+// }
